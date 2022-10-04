62da561a7c6811376481fda9df2517de26033f8e
AlexGuzmn <91238922+AlexGuzmn@users.noreply.github.com>
2022-10-03 22:02:14 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -6,7 +6,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_zero_args(self):
         self.assertEqual(stringcalculator.Add("1"), 1)
-        self.assertEqual(stringcalculator.Add(""), 1)
+        self.assertEqual(stringcalculator.Add(""), 0)
 
 
 if __name__ == '__main__':
