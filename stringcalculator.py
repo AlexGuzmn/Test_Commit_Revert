fa966dd25ea398005d0e37a1e2ddf7463d5353ab
AlexGuzmn <91238922+AlexGuzmn@users.noreply.github.com>
2022-10-03 22:51:57 -0500

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -3,19 +3,4 @@
 
 def Add(data):
   	#Add implementation
-	strLen = len(data)
-	sum = 0
-	aux = " "
-	counter = 0
-	while(counter < strLen):
-		try:
-			aux = data[counter]
-			if aux == "-":
-				counter = counter + 1
-			else:
-				sum = sum + int(aux)
-		except:
-			pass
-		counter = counter + 1
-	print(sum)
-	return sum
+	return 0
