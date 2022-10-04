d30a34778ed644ecdf026296b4a471700c3117f0
AlexGuzmn <91238922+AlexGuzmn@users.noreply.github.com>
2022-10-03 23:13:53 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -5,7 +5,7 @@ import stringcalculator
 class TestStringMethods(unittest.TestCase):
 
     def test_zero_args(self):
-        self.assertEqual(stringcalculator.Add("3;2;45"), 14)
+        self.assertEqual(stringcalculator.Add("31;2;45"), 15)
 
 
 if __name__ == '__main__':
