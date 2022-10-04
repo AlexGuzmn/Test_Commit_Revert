14c045daa2aa4e829ee94aece44a31c93d78bebe
AlexGuzmn <91238922+AlexGuzmn@users.noreply.github.com>
2022-10-03 22:21:25 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -5,7 +5,7 @@ import stringcalculator
 class TestStringMethods(unittest.TestCase):
 
     def test_zero_args(self):
-        self.assertEqual(stringcalculator.Add("11-2"), 4)
+        self.assertEqual(stringcalculator.Add("1-1-2"), 4)
 
 
 if __name__ == '__main__':
